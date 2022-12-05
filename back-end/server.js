97399687309c8928e485f76a44e5c2c5215254d7
Eyan Cea <eyan.moses@gmail.com>
2022-12-05 14:54:41 -0600

    back-end completed remplate

---
+++ back-end/server.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const app = express()
+const List = require('./models/DataList.js')
+
+app.use(express.json())
+app.use(cors())
+
+                            //Post route
+// app.post('/people', (req, res) => {
+//     console.log(req)
+//     People.create(req.body, (err, createdPerson) => {
+//       console.log(req.body)
+//       res.json(createdPerson)
+//     })
+//   })
+  
+                            //get route grabbing all data
+//   app.get('/people', (req, res) => {
+//     People.find({}, (err, foundPeople) => {
+//       res.json(foundPeople)
+//     })
+//   })
+  
+                            //delete route..
+//   app.delete('/people/:id', (req, res) => {
+//     People.findByIdAndRemove(req.params.id, (err, deletedPerson) => {
+//       res.json(deletedPerson)
+//     })
+//   })
+  
+                            //put route
+//   app.put('/people/:id', (req, res) => {
+//     People.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedPerson) => {
+//       res.json(updatedPerson)
+//     })
+//   })
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('listening...')
+ })
+ 
+ 
+ mongoose.connect('mongodb://localhost:27017/SongList')
+ mongoose.connection.once('open', () => {
+    console.log('connected to mongod...')
+ })
+
+ 
+
+ //Run in terminal to start. 
