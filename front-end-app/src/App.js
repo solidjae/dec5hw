29de6037a2c96f9ff8ed4c53e4700c4d2b20211f
solidjae <115513658+solidjae@users.noreply.github.com>
2022-12-06 14:00:42 -0600

    Update App.js
    finished app.js

--- front-end-app/src/App.js
+++ front-end-app/src/App.js
@@ -1,12 +1,10 @@
-// import logo from './logo.svg';
-// import './App.css';
 import {useState, useEffect} from 'react'
 import axios from 'axios'
 
 import Songs from './components/Songs'
 import Add from './components/Add'
 import Edit from './components/Edit'
-import List from '../../back-end/models/DataList'
+
 
 function App() {
 
@@ -76,3 +74,4 @@ function App() {
 }
 
 export default App;
+
